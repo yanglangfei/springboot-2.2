8379a8ffcb305eff334d021a9da1f23eef237579
yanglf <899117>
2019-10-19 14:27:51 +0800

    Initial commit

---
+++ src/main/java/com/yanglf/jdk11/mode/User.java
@@ -0,0 +1,12 @@
+package com.yanglf.jdk11.mode;
+
+/**
+ * @author : yanglf
+ * @version : 1.0
+ * @created IntelliJ IDEA.
+ * @date : 2019/10/19 13:52
+ * @desc :
+ */
+public class User {
+
+}
---
+++ src/test/java/com/yanglf/jdk11/Jdk11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanglf.jdk11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdk11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
